6a0b55da0165f109645c8d59663b1fee9f95db8a
Sang Nguyen <nnsang24@gmail.com>
2020-07-06 14:19:56 +0700

    Angular structural directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { CardImageComponent } from './components/card/card-image/card-image.component';
 import { HighlightedDirective } from './directives/highlighted.directive';
+import { NgxUnlessDirective } from './directives/ngx-unless.directive';
 
 @NgModule({
-	declarations: [AppComponent, CardComponent, CardImageComponent, HighlightedDirective],
+	declarations: [AppComponent, CardComponent, CardImageComponent, HighlightedDirective, NgxUnlessDirective],
 	imports: [BrowserModule],
 	providers: [],
 	bootstrap: [AppComponent],
